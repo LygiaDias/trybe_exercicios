7215ee4abb0b74ab8c088456ba50210425695050
Lygia Dias <lygiiadias@gmail.com>
2021-07-28 19:37:58 -0300

    Exercicio-02

---
+++ Fundamentos/Bloco-4-introducao-a-javascript/Dia_4.1-javacript-primeiros-passos/Exercício-02.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 8;
+let b = 4;
+
+if (a > b) {
+    console.log (" o valor de a é maior que o valor de b")
+} else { 
+    console.log ( " o valor de b é maior que o valor de a")
+}
