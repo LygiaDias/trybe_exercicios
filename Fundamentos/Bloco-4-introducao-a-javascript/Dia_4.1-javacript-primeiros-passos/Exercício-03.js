8faab0a2cdcdb35515808aebbb02571f9a7fe59f
Lygia Dias <lygiiadias@gmail.com>
2021-07-29 20:02:42 -0300

    Exercicio 4

--- Fundamentos/Bloco-4-introducao-a-javascript/Dia_4.1-javacript-primeiros-passos/Exercício-03.js
+++ Fundamentos/Bloco-4-introducao-a-javascript/Dia_4.1-javacript-primeiros-passos/Exercício-03.js
@@ -6,8 +6,7 @@ let c = 10;
 
 if (a > b && a > c ) {
     console.log ( 'a é o maior número')
-} else if 
-    (b > a && b > c) {
+} else if  (b > a && b > c) {
     console.log ('b é o maior número')
 } else {
         console.log ('c é o maior número')
